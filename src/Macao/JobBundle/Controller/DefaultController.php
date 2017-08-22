52dd968172ff2b7e106ca9a5531ae25f1bd780e2
Kevin Ta <ta_kevin@hotmail.fr>
2017-08-22 20:23:41 +0200

    Modified findBy functions

--- src/Macao/JobBundle/Controller/DefaultController.php
+++ src/Macao/JobBundle/Controller/DefaultController.php
@@ -12,8 +12,8 @@ class DefaultController extends Controller
 
     public function indexAction()
     {
-        $jobs = $this->em->getRepository('MacaoJobBundle:Job')->findAll();
-        $categories = $this->em->getRepository('MacaoJobBundle:Category')->findAll();
+        $jobs = $this->em->getRepository('MacaoJobBundle:Job')->findBy(array('published' => 1), array('createdAt' => 'DESC'));
+        $categories = $this->em->getRepository('MacaoJobBundle:Category')->findBy(array(), array('name' => 'ASC'));
 
         return $this->render('MacaoJobBundle:Default:index.html.twig', array(
             'jobs' => $jobs,
